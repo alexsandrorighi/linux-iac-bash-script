71e05631ea231cbfd2d50c179faf49d0491d2858
alexsandrorighi <alexsandro-righi@outlook.com>
2023-08-09 13:14:01 -0300

    up file script bash

---
+++ script-create-account.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+echo "Criando diretórios..."
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando grupo de usuários..."
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando usuários..."
+#substituir Senhaaqui por sua senha
+useradd carlos -m -s /bin/bash -p $(openssl passwd Senhaaqui) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd Senhaaqui) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd  Senhaaqui) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl passwd  Senhaaqui) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd  Senhaaqui) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd  Senhaaqui) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd Senhaaqui) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd  Senhaaqui) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd Senhaaqui) -G GRP_SEC
+
+echo "Ajustando permissões dos diretórios..."
+chown root:GRP_ADM /adm
+chown root:GRP_VEN  /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+
+echo "Executado com sucesso"
